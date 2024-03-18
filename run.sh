e17e76fc15316fbc72a480cc8198d506310aad76
Tom Haddad <thomas.haddad@ivy-solutions.de>
2024-03-18 08:29:50 +0100

    tidy

--- run.sh
+++ run.sh
@@ -6,9 +6,8 @@ docker build -t node-grpc-service -f services/node-grpc-service/Dockerfile .
 docker build -t node-http-service -f services/node-http-service/Dockerfile .
 kind load docker-image go-http-forwarder-service --name=$CLUSTER_NAME
 kind load docker-image go-http-receiver-service --name=$CLUSTER_NAME
-kind load docker-image go-grpc-service --name=$CLUSTER_NAME
-kind load docker-image go-grpc-service-2 --name=$CLUSTER_NAME
 kind load docker-image node-grpc-service --name=$CLUSTER_NAME
 kind load docker-image node-http-service --name=$CLUSTER_NAME
+kubectl apply -f ./kubernetes/namespaces.yaml
 kubectl apply -f ./kubernetes
 kubectl apply -f ./kubernetes/beyla
